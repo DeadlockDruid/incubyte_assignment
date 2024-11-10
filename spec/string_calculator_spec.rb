b4def4bdf0ac902a15be5fceafe96bb241e138e3
Divyanshu Rathore <divyanshu03@live.com>
2024-11-10 18:06:13 +0530

    Case 2: allow the Add method to handle an unknown amount of numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calculator.add('')).to eq(0)
     end
-    
+
     it 'returns the number itself for a single number' do
       expect(calculator.add('1')).to eq(1)
     end
-    
+
     it 'returns the sum of two numbers' do
       expect(calculator.add('1,2')).to eq(3)
+    end
+
+    it 'returns the sum of two or more numbers' do
+      expect(calculator.add('1,2,3,4,5,6,7,8,9,10')).to eq(55)
     end
   end
 end
