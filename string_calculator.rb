d8e022543eca1996d83ab255dc19e637acaf8e7f
Divyanshu Rathore <divyanshu03@live.com>
2024-11-10 18:15:30 +0530

    Case 3: allow the Add method to handle new lines between numbers (instead of commas)

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers_string)
     return 0 if numbers_string.empty?
 
-    numbers_string.split(',').map(&:to_i).reduce(0, :+)
+    numbers_string.gsub('\\n', ',').split(',').map(&:to_i).reduce(0, :+)
   end
 end
